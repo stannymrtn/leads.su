46568a6c8e4f63b041458ff8f9669e0901d056d7
Timur <Stanmr7n@gmail.com>
2025-05-02 14:36:16 +0700

    edit port number in command executor

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -36,7 +36,7 @@ def setup_browser(request):
     options.capabilities.update(selenoid_capabilities)
 
     driver = webdriver.Remote(
-        command_executor=f"http://localhost:4444/wd/hub",
+        command_executor='http://localhost:4444/wd/hub',
         options=options
     )
 
